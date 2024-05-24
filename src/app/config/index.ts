594f97503a686fd85521d4691fe986e4f7c3c593
fecrakib <rakibislam132575@gmail.com>
2024-05-24 15:12:27 +0600

    connection work properly

---
+++ src/app/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path:path.resolve (__dirname,'../../../.env')})
+
+console.log('PORT:', process.env.PORT); // Add this line
+console.log('DB_URL:', process.env.DATABASE_URL); // Add this line
+
+export default {
+    port:process.env.PORT,
+    db_url:process.env.DATABASE_URL
+}
