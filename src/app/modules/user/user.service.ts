8367bf0d554099b4a543e12ae83f28e7a3729755
fecrakib <rakibislam132575@gmail.com>
2024-05-25 21:42:20 +0600

    new student create

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,28 +1,43 @@
-import config from "../../config";
-import { TStudent } from "../student/student.controller";
-import { Student } from "../student/student.model";
-import { TUser } from "./user.interface";
-import { User } from "./user.model";
+import config from '../../config';
+import { Student } from '../student/student.model';
+import { TStudent } from './../student/student.interface';
+import { TUser } from './user.interface';
+import { User } from './user.model';
+
+
+
 
 
 export const createStudentIntoDB = async (password:string,studentData:TStudent)=>{
-    // create a user object
-    const userData:Partial <TUser> ={};
-    // if password is not given , use default password
-    userData.password = password || (config.default_password as string);
-    // set manually generate id
-    userData.id='2030100001';
-    // create a user
-    const newUser = await User.create(userData);
+    //create a user object
+const userData:Partial <TUser> ={};
+ //set password 
+ userData.password = password || (config.default_password as string)
+ 
+ // set student role
 
-    // create a student
+ userData.role='student';
 
-    if(Object.keys(newUser).length){
+ //set manually generate id
 
-        // set id, _id as user
-        studentData.id= newUser.id;
-        studentData.user= newUser._id; //reference _id
-        const newStudent =await Student.create(studentData);
-        return newStudent;
-    }
+ userData.id= '20021323'
+
+ // create user
+
+ const newUser = await User.create(userData);
+
+ // create a student 
+ 
+ if(Object.keys(newUser).length){
+    
+    // set id, _id as user
+
+    studentData.id=newUser.id;
+    studentData.user= newUser._id //reference id
+    const newStudent = await Student.create(studentData);
+    return newStudent;
+ }
+
+
+
 }
