f45e89bc951b32f5c04fdf0c928f896821eee9d6
fecrakib <rakibislam132575@gmail.com>
2024-06-02 17:55:28 +0600

    globalError handle done

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,6 +7,7 @@ console.log('PORT:', process.env.PORT); // Add this line
 console.log('DB_URL:', process.env.DATABASE_URL); // Add this line
 
 export default {
+    NODE_ENV: process.env.NODE_ENV,
     port:process.env.PORT,
     db_url:process.env.DATABASE_URL,
     bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
